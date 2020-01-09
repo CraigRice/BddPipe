33812d8ab9102bb83b719c8d3c0f298f74ad66fc
CraigRice <rice.i.craig@gmail.com>
2020-01-09 19:09:33 +1000

    Update recipe extensions and documentation.

--- BddPipe/BddPipe/Recipe/RecipeExtensions.cs
+++ BddPipe/BddPipe/Recipe/RecipeExtensions.cs
@@ -9,48 +9,35 @@ namespace BddPipe.Recipe
     public static class RecipeExtensions
     {
         /// <summary>
-        /// 
+        /// Add a recipe in place of the 'Given' step
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="pipe"></param>
-        /// <param name="recipeFunction"></param>
-        /// <returns></returns>
-        public static Pipe<T> GivenRecipe<T>(
-            this Scenario pipe,
-            Func<
-                Scenario,
-                Pipe<T>
-            > recipeFunction) => recipeFunction(pipe);
+        /// <typeparam name="R">Type to be represented by the <see cref="Pipe{R}"/> after the step(s) are run.</typeparam>
+        /// <param name="scenario">Current <see cref="Scenario"/> instance to add step(s) to.</param>
+        /// <param name="recipeFunction">Function describing the step(s) added to the <see cref="Scenario"/></param>
+        /// <returns>A <see cref="Pipe{R}"/> with recipe steps applied via the supplied function.</returns>
+        public static Pipe<R> GivenRecipe<R>(this Scenario scenario, Func<Scenario, Pipe<R>> recipeFunction) =>
+            recipeFunction(scenario);
 
         /// <summary>
         /// 
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <typeparam name="R"></typeparam>
-        /// <param name="pipe"></param>
-        /// <param name="recipeFunction"></param>
-        /// <returns></returns>
-        public static Pipe<R> AndRecipe<T, R>(
-            this Pipe<T> pipe,
-            Func<
-                Pipe<T>,
-                Pipe<R>
-            > recipeFunction) => recipeFunction(pipe);
+        /// <typeparam name="T">Initial incoming type represented by the <see cref="Pipe{T}"/></typeparam>
+        /// <typeparam name="R">Type to be represented by the <see cref="Pipe{R}"/> after the step(s) are run.</typeparam>
+        /// <param name="pipe">Current <see cref="Pipe{T}"/> instance to add step(s) to.</param>
+        /// <param name="recipeFunction">Function describing the step(s) added to the <see cref="Pipe{T}"/></param>
+        /// <returns>A <see cref="Pipe{R}"/> with recipe steps applied via the supplied function.</returns>
+        public static Pipe<R> AndRecipe<T, R>(this Pipe<T> pipe,  Func<Pipe<T>, Pipe<R>> recipeFunction) =>
+            recipeFunction(pipe);
 
         /// <summary>
         /// 
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <typeparam name="R"></typeparam>
-        /// <param name="pipe"></param>
-        /// <param name="recipeFunction"></param>
-        /// <returns></returns>
-        public static Pipe<R> ThenRecipe<T, R>(
-            this Pipe<T> pipe,
-            Func<
-                Pipe<T>,
-                Pipe<R>
-            > recipeFunction) => recipeFunction(pipe);
-
+        /// <typeparam name="T">Initial incoming type represented by the <see cref="Pipe{T}"/></typeparam>
+        /// <typeparam name="R">Type to be represented by the <see cref="Pipe{R}"/> after the step(s) are run.</typeparam>
+        /// <param name="pipe">Current <see cref="Pipe{T}"/> instance to add step(s) to.</param>
+        /// <param name="recipeFunction">Function describing the step(s) added to the <see cref="Pipe{T}"/></param>
+        /// <returns>A <see cref="Pipe{R}"/> with recipe steps applied via the supplied function.</returns>
+        public static Pipe<R> ThenRecipe<T, R>(this Pipe<T> pipe, Func<Pipe<T>, Pipe<R>> recipeFunction) => 
+            recipeFunction(pipe);
     }
 }
