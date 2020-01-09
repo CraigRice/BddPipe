9fd1602c955526145224485d030197fdfa2dbbb5
CraigRice <rice.i.craig@gmail.com>
2020-01-09 19:11:35 +1000

    Update documentation.

--- BddPipe/BddPipe/Recipe/RecipeExtensions.cs
+++ BddPipe/BddPipe/Recipe/RecipeExtensions.cs
@@ -9,7 +9,7 @@ namespace BddPipe.Recipe
     public static class RecipeExtensions
     {
         /// <summary>
-        /// Add a recipe in place of the 'Given' step
+        /// Allow a function to run in place of the 'Given' step to add a reusable step or series of steps based on its implementation.
         /// </summary>
         /// <typeparam name="R">Type to be represented by the <see cref="Pipe{R}"/> after the step(s) are run.</typeparam>
         /// <param name="scenario">Current <see cref="Scenario"/> instance to add step(s) to.</param>
@@ -19,7 +19,7 @@ namespace BddPipe.Recipe
             recipeFunction(scenario);
 
         /// <summary>
-        /// 
+        /// Allow a function to run in place of the 'And' step to add a reusable step or series of steps based on its implementation.
         /// </summary>
         /// <typeparam name="T">Initial incoming type represented by the <see cref="Pipe{T}"/></typeparam>
         /// <typeparam name="R">Type to be represented by the <see cref="Pipe{R}"/> after the step(s) are run.</typeparam>
@@ -30,13 +30,13 @@ namespace BddPipe.Recipe
             recipeFunction(pipe);
 
         /// <summary>
-        /// 
+        /// Allow a function to run in place of the 'Then' step to add a reusable step or series of steps based on its implementation.
         /// </summary>
         /// <typeparam name="T">Initial incoming type represented by the <see cref="Pipe{T}"/></typeparam>
         /// <typeparam name="R">Type to be represented by the <see cref="Pipe{R}"/> after the step(s) are run.</typeparam>
         /// <param name="pipe">Current <see cref="Pipe{T}"/> instance to add step(s) to.</param>
         /// <param name="recipeFunction">Function describing the step(s) added to the <see cref="Pipe{T}"/></param>
-        /// <returns>A <see cref="Pipe{R}"/> with recipe steps applied via the supplied function.</returns>
+        /// <returns>A <see cref="Pipe{R}"/> instance as a result of the recipe function.</returns>
         public static Pipe<R> ThenRecipe<T, R>(this Pipe<T> pipe, Func<Pipe<T>, Pipe<R>> recipeFunction) => 
             recipeFunction(pipe);
     }
