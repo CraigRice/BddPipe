f72935d9b8fd23b87d6474cb175c9323241e9bae
Craig Rice <rice.i.craig@gmail.com>
2021-10-04 21:32:43 +1000

    Move map tests.

--- BddPipe/BddPipe.UnitTests/RunnerMapTests.cs
+++ BddPipe/BddPipe.UnitTests/RunnerMapTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.ExceptionServices;
 using System.Threading.Tasks;
 using BddPipe.Model;
@@ -8,12 +10,24 @@ using FluentAssertions;
 using NSubstitute;
 using NUnit.Framework;
 using static BddPipe.F;
+using static BddPipe.Runner;
 
 namespace BddPipe.UnitTests
 {
     [TestFixture]
     public class RunnerMapTests
     {
+        private static IReadOnlyList<string> WriteLogsToConsole(ScenarioResult result)
+        {
+            Runner.WriteLogsToConsole(result);
+
+            return
+                (result.Title == null ? Array.Empty<string>() : new[] { result.Description })
+                .Concat(result.StepResults
+                .Select(l => l.Description))
+                .ToList();
+        }
+
         private const int InitialValue = 1;
         private const int NewValue = 2;
         private const string ErrorMessage = "test message";
@@ -255,6 +269,201 @@ namespace BddPipe.UnitTests
                 ctnError.Content.SourceException.Should().BeOfType<InconclusiveException>();
                 ctnError.StepOutcomes.ShouldHaveSingleStepOutcome(Outcome.Inconclusive, givenStepText, Step.Given);
             });
+        }
+
+        [Test]
+        public void Map_AfterGivenWhen_PipeIsInCorrectState()
+        {
+            const string givenTitle = "the given title";
+            const string whenTitle = "the when title";
+            const string valueInComplexType = "the string value";
+
+            var pipe = Scenario()
+                .Given(givenTitle, () => { })
+                .When(whenTitle, () => new { A = 1, B = valueInComplexType })
+                .Map(stepValue => stepValue.B);
+
+            pipe.ShouldBeSuccessfulSecondStepWithValue(Step.When, givenTitle, whenTitle, valueInComplexType);
+        }
+
+        [Test]
+        public void Map_AsyncMap_OutputIsCorrect()
+        {
+            IReadOnlyList<string> logLines = new List<string>();
+
+            Action runTest = () =>
+                Scenario()
+                    .Given("some text", () => "some text")
+                    .When("the text is altered", text => text.Substring(0, 4))
+                    .Map(async text =>
+                    {
+                        await Task.Delay(10);
+                        return text.Length;
+                    })
+                    .Then("text length should be as expected", arg =>
+                    {
+                        arg.Should().Be(4);
+                    })
+                    .Run(logs => logLines = WriteLogsToConsole(logs));
+
+            runTest.Should().NotThrow();
+
+            logLines.Count.Should().Be(4);
+            logLines[0].Should().Be("Scenario: Map_AsyncMap_OutputIsCorrect");
+            logLines[1].Should().Be("  Given some text [Passed]");
+            logLines[2].Should().Be("  When the text is altered [Passed]");
+            logLines[3].Should().Be("  Then text length should be as expected [Passed]");
+        }
+
+        [Test]
+        public void Map_SyncMap_OutputIsCorrect()
+        {
+            IReadOnlyList<string> logLines = new List<string>();
+
+            Action runTest = () =>
+                Scenario()
+                    .Given("some text", () => "some text")
+                    .When("the text is altered", text => text.Substring(0, 4))
+                    .Map(text => text.Length)
+                    .Then("text length should be as expected", arg =>
+                    {
+                        arg.Should().Be(4);
+                    })
+                    .Run(logs => logLines = WriteLogsToConsole(logs));
+
+            runTest.Should().NotThrow();
+
+            logLines.Count.Should().Be(4);
+            logLines[0].Should().Be("Scenario: Map_SyncMap_OutputIsCorrect");
+            logLines[1].Should().Be("  Given some text [Passed]");
+            logLines[2].Should().Be("  When the text is altered [Passed]");
+            logLines[3].Should().Be("  Then text length should be as expected [Passed]");
+        }
+
+        [Test]
+        public void Map_DoubleMap_OutputIsCorrect()
+        {
+            IReadOnlyList<string> logLines = new List<string>();
+
+            Action runTest = () =>
+                Scenario()
+                    .Given("some text", () => "some text")
+                    .When("the text is altered", text => text.Substring(0, 4))
+                    .Map(text => text.Length)
+                    .Map(length => length.ToString())
+                    .Then("text length should be as expected", arg =>
+                    {
+                        arg.Should().Be("4");
+                    })
+                    .Run(logs => logLines = WriteLogsToConsole(logs));
+
+            runTest.Should().NotThrow();
+
+            logLines.Count.Should().Be(4);
+            logLines[0].Should().Be("Scenario: Map_DoubleMap_OutputIsCorrect");
+            logLines[1].Should().Be("  Given some text [Passed]");
+            logLines[2].Should().Be("  When the text is altered [Passed]");
+            logLines[3].Should().Be("  Then text length should be as expected [Passed]");
+        }
+
+        [Test]
+        public void Map_AsyncMapThrowsInconclusiveException_OutputIsCorrect()
+        {
+            IReadOnlyList<string> logLines = new List<string>();
+
+            Action runTest = () =>
+                Scenario()
+                    .Given("some text", () => "some text")
+                    .When("the text is altered", text => text.Substring(0, 4))
+                    .Map(PipeMapFunctions.MapAsyncRaiseInconclusiveEx())
+                    .Then("text length should be as expected", arg =>
+                    {
+                        arg.Should().Be(15);
+                    })
+                    .Run(logs => logLines = WriteLogsToConsole(logs));
+
+            runTest.Should().Throw<InconclusiveException>();
+
+            logLines.Count.Should().Be(4);
+            logLines[0].Should().Be("Scenario: Map_AsyncMapThrowsInconclusiveException_OutputIsCorrect");
+            logLines[1].Should().Be("  Given some text [Passed]");
+            logLines[2].Should().Be("  When the text is altered [Inconclusive]");
+            logLines[3].Should().Be("  Then text length should be as expected [not run]");
+        }
+
+        [Test]
+        public void Map_SyncMapThrowsInconclusiveException_OutputIsCorrect()
+        {
+            IReadOnlyList<string> logLines = new List<string>();
+
+            Action runTest = () =>
+                Scenario()
+                    .Given("some text", () => "some text")
+                    .When("the text is altered", text => text.Substring(0, 4))
+                    .Map(PipeMapFunctions.MapSyncRaiseInconclusiveEx())
+                    .Then("text length should be as expected", arg =>
+                    {
+                        arg.Should().Be(15);
+                    })
+                    .Run(logs => logLines = WriteLogsToConsole(logs));
+
+            runTest.Should().Throw<InconclusiveException>();
+
+            logLines.Count.Should().Be(4);
+            logLines[0].Should().Be("Scenario: Map_SyncMapThrowsInconclusiveException_OutputIsCorrect");
+            logLines[1].Should().Be("  Given some text [Passed]");
+            logLines[2].Should().Be("  When the text is altered [Inconclusive]");
+            logLines[3].Should().Be("  Then text length should be as expected [not run]");
+        }
+
+        [Test]
+        public void Map_AsyncMapThrowsException_OutputIsCorrect()
+        {
+            IReadOnlyList<string> logLines = new List<string>();
+
+            Action runTest = () =>
+                Scenario()
+                    .Given("some text", () => "some text")
+                    .When("the text is altered", text => text.Substring(0, 4))
+                    .Map(PipeMapFunctions.MapAsyncRaiseEx())
+                    .Then("text length should be as expected", arg =>
+                    {
+                        arg.Should().Be(15);
+                    })
+                    .Run(logs => logLines = WriteLogsToConsole(logs));
+
+            runTest.Should().Throw<DivideByZeroException>();
+
+            logLines.Count.Should().Be(4);
+            logLines[0].Should().Be("Scenario: Map_AsyncMapThrowsException_OutputIsCorrect");
+            logLines[1].Should().Be("  Given some text [Passed]");
+            logLines[2].Should().Be("  When the text is altered [Failed]");
+            logLines[3].Should().Be("  Then text length should be as expected [not run]");
+        }
+
+        [Test]
+        public void Map_SyncMapThrowsException_OutputIsCorrect()
+        {
+            IReadOnlyList<string> logLines = new List<string>();
+
+            Action runTest = () =>
+                Scenario()
+                    .Given("some text", () => "some text")
+                    .When("the text is altered", text => text.Substring(0, 4))
+                    .Map(PipeMapFunctions.MapSyncRaiseEx())
+                    .Then("text length should be as expected", arg =>
+                    {
+                        arg.Should().Be(15);
+                    })
+                    .Run(logs => logLines = WriteLogsToConsole(logs));
+
+            runTest.Should().Throw<DivideByZeroException>();
+
+            logLines.Count.Should().Be(4);
+            logLines[0].Should().Be("Scenario: Map_SyncMapThrowsException_OutputIsCorrect");
+            logLines[1].Should().Be("  Given some text [Passed]");
+            logLines[2].Should().Be("  When the text is altered [Failed]");
+            logLines[3].Should().Be("  Then text length should be as expected [not run]");
         }
     }
 }
