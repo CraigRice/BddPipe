2302fb5664c4bd6aaeb569cce4afd4565bc03ff7
CraigRice <rice.i.craig@gmail.com>
2025-03-30 20:36:14 +1000

    Fix merge.

--- BddPipe/BddPipe/Model/Ctn.cs
+++ BddPipe/BddPipe/Model/Ctn.cs
@@ -27,46 +27,4 @@ namespace BddPipe.Model
             ScenarioTitle = scenarioTitle;
         }
     }
-
-    internal static class CtnExtensions
-    {
-        /// <summary>
-        /// Projects from one value to another and does not impact current step progress.
-        /// </summary>
-        /// <typeparam name="T">Current type</typeparam>
-        /// <typeparam name="R">Type of the resulting value</typeparam>
-        /// <param name="ctn">The <see cref="Ctn{T}"/> instance to perform this operation on.</param>
-        /// <param name="map">A function to map the current value to its new value.</param>
-        /// <returns>A new <see cref="Ctn{T}"/> instance of the destination type</returns>
-        public static Ctn<R> Map<T, R>(this Ctn<T> ctn, Func<T, R> map)
-        {
-            if (map == null) { throw new ArgumentNullException(nameof(map)); }
-
-            return new Ctn<R>(map(ctn.Content), ctn.StepOutcomes, ctn.ScenarioTitle);
-        }
-
-        public static async Task<Ctn<R>> MapAsync<T, R>(this Ctn<T> ctn, Func<T, Task<R>> map)
-        {
-            if (map == null) { throw new ArgumentNullException(nameof(map)); }
-
-            var content = await map(ctn.Content).ConfigureAwait(false);
-
-            return new Ctn<R>(content, ctn.StepOutcomes, ctn.ScenarioTitle);
-        }
-
-        public static Ctn<R> ToCtn<T, R>(this Ctn<T> ctn, R newContent, in Some<StepOutcome> withStepOutcome)
-        {
-            var outcomes = new List<StepOutcome>(ctn.StepOutcomes) { withStepOutcome.Value };
-            return new Ctn<R>(newContent, outcomes, ctn.ScenarioTitle);
-        }
-
-        private const string ScenarioPrefix = "Scenario:";
-
-        public static Some<ScenarioResult> ToResult<T>(this Ctn<T> ctn) =>
-            new ScenarioResult(
-                title: ctn.ScenarioTitle.IfNone(null),
-                description: ctn.ScenarioTitle.NoneIfWhiteSpace().WithPrefix(ScenarioPrefix),
-                stepResults: ctn.StepOutcomes.ToResults(ctn.ScenarioTitle.IsSome)
-            );
-    }
 }
